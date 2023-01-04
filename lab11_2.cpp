c2953a1a8368c35c376bbdaa6520a7740fed8537
Vasiree Nuntajan <wasiree098@gmail.com>
2023-01-04 21:03:30 +0700

    lab11_2 ok

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -9,8 +9,13 @@ int main (){
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+	
+	dest << "-------------------- BOOM ---------------------\n";
+	string textline;
+	while (getline(source,textline)){
+	  dest << textline << '\n';
+	}
+	dest << "-------------------- HA!! ---------------------";
 
     source.close();
     dest.close();
